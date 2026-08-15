8533014d2024304a55797e13be12943bfbdc71d8
Granvallen <granvallen314@163.com>
2026-08-15 16:40:12 +0800

    Site updated: 2026-08-15 16:39:51

--- js/fancybox.js
+++ js/fancybox.js
@@ -1,19 +1,15 @@
 $(document).ready(function() {
-  $('img').each(function() {
+  $('.post-content img').each(function() {
     if ($(this).parent().hasClass('fancybox')) return;
     if ($(this).hasClass('nofancybox')) return;
     var alt = this.alt;
     if (alt) $(this).after('<span class="caption">' + alt + '</span>');
-    $(this).wrap('<a href="' + ($(this).attr('data-src') == null ? this.src : $(this).attr('data-src')) + '" title="' + alt + '" class="fancybox"></a>');
+    $(this).wrap('<a href="' + ($(this).attr('data-src') == null ? this.src : $(this).attr('data-src')) + '" class="fancybox"></a>');
   });
-  $(this).find('.fancybox').each(function(){
-    $(this).attr('rel', 'article');
-  });
-});
-$(document).ready(function() {
-  $("a[href$='.jpg'],a[href$='.png'],a[href$='.gif'],a[href$='.webp']").attr('rel', 'gallery').fancybox({
-    helpers : {
-    title: { type: 'inside'}
-    }
+
+  $('.post-content a.fancybox').each(function() {
+    var image = $(this).find('img').first();
+    $(this).attr('data-fancybox', 'article');
+    if (image.attr('alt')) $(this).attr('data-caption', image.attr('alt'));
   });
 });
